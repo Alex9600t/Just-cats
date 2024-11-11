9ac166df35f49b88a952f402f9aad2bf2ab48863
Alex9600t <dukovaleks2@gmail.com>
2024-11-12 02:57:11 +0300

    feat(Database+client): added database and updated client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,13 @@ import './App.css'
 function App() {
   const [CaT, setCaT] = useState('');
   const [cAT, setcAT] = useState('');
+  const [cat, setcat] = useState('');
+  const [catImage, setCatImage] = useState('');
+  const [caterror, setCatError] = useState('');
   const [CAt, setCAt] = useState('');
+  const [CATStyle, setCATStyle] = useState("none")
+  const [cATDecor, setcATDecor] = useState('')
+  const [cATMax, setcATMax] = useState('')
 
   useEffect(() => {
     cATURL()
@@ -13,30 +19,129 @@ function App() {
   const cATURL = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const value = urlParams.get('id');
-    setcAT(value);
+    setcAT(Number(value));
   }
-  useEffect(() => {
-    if (cAT === 'hi') {
-      console.log('hi');
-    } else if (cAT === 'hello') {
-      console.log('Meow');
+
+  const cATBack = () => {
+    if (cAT !== 1) {
+      return `${cAT - 1} <-`
+    } else if (cAT < 1) {
+      return `-> 1 <-`
     } else {
-      console.log(cAT);
+      return `..`
+    }
+  }
+
+  const cATBackOnClick = () => {
+    if (cAT < 1) {
+      window.location.href = `${new URL(document.URL).origin}?id=${cATMax}`;
+    } else if (cAT !== 1) {
+      window.location.href = `${new URL(document.URL).origin}?id=${cAT - 1}`;
+    } else {
+    }
+  }
+
+  const cATNext = () => {
+    if (cAT > cATMax) {
+      return `-> ${cATMax} <-`
+    } else if (cAT !== cATMax) {
+      return `-> ${cAT + 1}`
+    } else {
+      return `..`
+    }
+  }
+
+  const cATNextOnClick = () => {
+    if (cAT > cATMax) {
+      window.location.href = `${new URL(document.URL).origin}?id=${cATMax}`;
+    } else if (cAT !== cATMax) {
+      window.location.href = `${new URL(document.URL).origin}?id=${cAT + 1}`;
+    } else {
+    }
+  }
+
+  const cATURLError = async () => {
+    const url = `http://localhost:3000/errorCats`;
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Ошибка: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const cATURLInfo = async (cAT) => {
+    const url = `http://localhost:3000/cats?id=${cAT}`;
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Ошибка: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setCaT(`cat name: ${data.name}`);
+      setCAt(`cat description: ${data.description}`);
+      setcat(`author: ${data.author}`)
+      setcATDecor(`id: ${data.id}`)
+      setcATMax(`${Number(data.maxid)}`)
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // const cATURLImage = (cAT) => {
+  //   cATURLImage = `http://localhost:3000/imagesCats?id=${cAT}`;
+
+  // };
+
+  useEffect(() => {
+    if (cAT) {
+      cATURLInfo(cAT);
+      setCATStyle('none');
+      // cATURLImage(cAT)
+    } else {
+      setCATStyle("block")
+      cATURLError()
     }
   }, [cAT]);
 
   return (
     <>
+      <div style={{ display: CATStyle }}>
+        <img src="http://localhost:3000/errorCats" alt="" />
+      </div>
       <div>
         <div className="catimg">
-          
+          <img src={`http://localhost:3000/imagesCats?id=${cAT}`} alt="" />
         </div>
         <div className="catinfo">
-          <h1>cat name: {CaT}</h1>
-          <p>cat description: {CAt}</p>
+          <h1>{CaT}</h1>
+          <p>
+            {CAt}
+          </p>
+          <p>
+            {cat}
+          </p>
           <span>
-            id: {cAT}
+            {cATDecor}
           </span>
+        </div>
+        <div>
+          <button onClick={cATBackOnClick}>
+            {cATBack()}
+          </button>
+          {cAT}
+          <button onClick={cATNextOnClick}>
+            {cATNext()}
+          </button>
         </div>
       </div>
     </>
--- server/index.js
+++ server/index.js
@@ -6,12 +6,37 @@ const app = express();
 
 // //! API
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 app.use(express.json());
 
+app.get('/errorCats', (req, res) => {
+    const id = Math.floor(Math.random() * 10) + 1;
+
+    getCatsError404(id)
+        .then((image) => {
+            if (image) {
+                res.setHeader('Content-Type', 'image/png');
+                res.send(image);
+            } else {
+                res.status(404).json({ message: 'Cat not found' });
+            }
+        })
+        .catch((error) => {
+            console.error('Ошибка:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        });
+});
+
 app.get('/imagesCats', (req, res) => {
     const id = req.query.id;
-    
+
     if (id) {
         getImageById(id)
             .then((image) => {
@@ -41,7 +66,13 @@ app.get('/cats', (req, res) => {
                     .then((description) => {
                         getAuthorById(id)
                             .then((author) => {
-                                res.json({ name: `${name}`, description: `${description}`, author: `${author}` });
+                                getMaxId()
+                                    .then((maxid) => {
+                                        res.json({ name: `${name}`, description: `${description}`, author: `${author}`, id: `${id}`, maxid: `${maxid}` });
+                                    })
+                                    .catch((error) => {
+                                        console.error('Ошибка:', error);
+                                    })
                             })
                             .catch((error) => {
                                 console.error('Ошибка:', error);
@@ -129,5 +160,38 @@ function getAuthorById(id) {
     });
 }
 
+function getCatsError404(id) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT image FROM errorCats WHERE id = ?`;
+
+        db.get(query, [id], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row ? row.image : null);
+            }
+        });
+    });
+}
+
+function getMaxId() {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT MAX(id) AS last_id FROM cats`;
+
+        db.get(query, (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row ? row.last_id : null);
+            }
+        });
+    });
+}
+
+// Hiii!!
+// How are u??
 
 // //! Bin to PNG
+
+// btw, ТВОЙ КОД ПОЛНАЯ ХУЙНЯ
+// p.s. пласти >:(
